cfe06ef2e518bd4602f55d7329696ba0eefb5b7a
Leandro Favio Gomez Racca <leo1ragomez95@gmail.com>
2020-11-19 14:06:26 +0100

    Main for launch CLI

---
+++ src/oop/Main.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Main {
+    public static void main(String[] args) {
+        int exitCode;
+        System.out.println("Start Main");
+        System.out.println("Start CLI");
+        Cli program = new Cli();
+        exitCode = program.Run();
+        System.out.println("Finish CLI, exit Code: " + exitCode);
+    }
+}
