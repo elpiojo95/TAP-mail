eb2cd9318d53bb5a28481eaa5c1f831b8b0aa15c
Leandro Favio Gomez Racca <leo1ragomez95@gmail.com>
2020-11-19 15:18:21 +0100

    added selectMailstore

--- src/oop/Cli.java
+++ src/oop/Cli.java
@@ -1,11 +1,38 @@
 package oop;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Cli {
 
     public Cli() {
     }
 
-    public int Run() {
+    public int run() {
+        switch (selectMailstore()) {
+            case 1 :
+
+                break;
+            case 2 :
+                break;
+        }
         return 0;
+    }
+
+    public int selectMailstore() {
+        System.out.println("1. Memory system\n" +
+                "2. File system");
+        Scanner scanner = new Scanner(System.in);
+        boolean validInteger = false;
+        int option = 0;
+        while (!validInteger) {
+            try {
+                option= scanner.nextInt();
+                if (option==1 || option==2) validInteger = true;
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+            }
+        }
+        return option;
     }
 }
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Start Main");
         System.out.println("Start CLI");
         Cli program = new Cli();
-        exitCode = program.Run();
+        exitCode = program.run();
         System.out.println("Finish CLI, exit Code: " + exitCode);
     }
 }
