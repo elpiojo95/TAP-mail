399f392153972f92576c392eeaa4b3982e084f4a
Miriam Gertrudix <miriam.gertrudix@estudiants.urv.cat>
2020-12-10 09:48:14 +0100

    MemoryMailStore implemented and tested Mailbox updated and connected to MailStore MailStore implemented and FileMailStore in process

--- src/oop/FileMailStore.java
+++ src/oop/FileMailStore.java
@@ -1,4 +1,18 @@
 package oop;
 
+import java.util.List;
+
 public class FileMailStore implements MailStore{
+
+
+    public void send(Message msg){
+
+    }
+
+    public List<Message> get(String username){
+
+        return null;
+    }
+
+
 }
